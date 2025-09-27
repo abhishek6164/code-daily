494d4e9bacf52ea4bfe2ffa05160d61f4ae53d28
abhishek6164 <abhishekpipriye2507@gmail.com>
2025-09-27 11:53:33 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,5 +1,54 @@
-function abhishek() {
-   console.log("Hello Abhishek");
-}
+// function abhishek() {
+//    console.log("Hello Abhishek");
+// }
 
-abhishek();
+// abhishek();
+
+
+//  ** function statements **
+// function abhishek() {
+
+// }
+// abhishek();
+
+
+
+//  ** function expression **
+// let abhishek = function () {
+//     console.log("Hello Abhishek");
+// }
+
+// abhishek();
+
+
+//  ** fat arrow function **
+// let abhishek = () => {
+//     console.log("Hello Abhishek");
+// }
+// abhishek();
+
+//  ** fat arrow function with parameters **
+// let abhishek = (name) => {
+//     console.log("Hello " + name);
+// }
+// abhishek("Abhishek");
+
+
+
+// function abhishek(name) {
+//     console.log("Hello" + name)
+
+// }
+
+// abhishek(" Abhishek");
+
+// function dance(name) {
+//     console.log(`${name} is dancing`);
+// }
+
+// dance("Abhishek");
+// dance("Rohit");
+// dance("Shubham");
+// dance("Sagar");
+// dance("Ankit");
+// dance("Ravi");
